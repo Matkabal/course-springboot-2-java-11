8371bf2d93bf81fa9b27de22af4cc8e4d2d80627
Matkabal <mateuslima200919@gmail.com>
2021-04-07 11:52:53 -0300

    User entity and resource

---
+++ src/main/java/com/matkabal/course/resourses/UserResource.java
@@ -0,0 +1,21 @@
+package com.matkabal.course.resourses;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matkabal.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1l, "Mateus", "mateus@gmail.com", "85717185", "1234567");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}

69b97d1d1980518002bba1926c8eacea4af9db7f
Matkabal <mateuslima200919@gmail.com>
2021-04-08 16:54:07 -0300

    Service layer, component registration and Order, Instant, ISO 8601

---
+++ src/main/java/com/matkabal/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.matkabal.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matkabal.course.entities.User;
+import com.matkabal.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
+

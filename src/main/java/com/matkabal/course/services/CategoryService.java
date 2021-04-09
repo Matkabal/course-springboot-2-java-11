93564bb451d2f734f59a4693a405c0e4d28ad7c4
Matkabal <mateuslima200919@gmail.com>
2021-04-09 18:51:09 -0300

    Category

---
+++ src/main/java/com/matkabal/course/services/CategoryService.java
@@ -0,0 +1,31 @@
+package com.matkabal.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matkabal.course.entities.Category;
+import com.matkabal.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
+
